c0d62c1a905fb006e0155a274b5c5b0ab1010d14
David Fox <djwfox@gmail.com>
2024-03-07 14:38:58 +0000

    Step 4 of wiki - movement of character

--- scene/main/InitWorld.gd
+++ scene/main/InitWorld.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal sprite_created(new_sprite)
+
 @onready var Player := preload("res://sprite/PC.tscn") as PackedScene
 @onready var Wall := preload("res://sprite/Wall.tscn") as PackedScene
 @onready var Floor := preload("res://sprite/Floor.tscn") as PackedScene
@@ -7,11 +9,12 @@ extends Node2D
 var _new_GroupName := preload("res://library/GroupName.gd").new()
 var _new_ConvertCoord := preload("res://library/ConvertCoord.gd").new()
 var _new_DungeonSize := preload("res://library/DungeonSize.gd").new()
+var _new_InputName := preload("res://library/InputName.gd").new()
+var _initialized: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_create_walls_and_floor()
-	_create_player()
+	pass
 
 func _create_sprite(prefab: PackedScene, group: String, x: int, y: int,
 		x_offset: int = 0, y_offset: int = 0) -> void:
@@ -20,6 +23,8 @@ func _create_sprite(prefab: PackedScene, group: String, x: int, y: int,
 	sprite.add_to_group(group)
 
 	add_child(sprite)
+
+	sprite_created.emit(sprite)
 
 func _create_player():
 	_create_sprite(Player, _new_GroupName.PC, 1, 1)
@@ -34,5 +39,13 @@ func _create_walls_and_floor():
 				_create_sprite(Floor, _new_GroupName.Building, x, y)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta) -> void:
 	pass
+
+func _unhandled_input(event):
+	if event.is_action_pressed(_new_InputName.INIT_WORLD):
+		if not _initialized:
+			_initialized = true
+			_create_walls_and_floor()
+			_create_player()
+			set_process_unhandled_input(false)
---
+++ scene/main/PCMove.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+var _new_InputName := preload("res://library/InputName.gd").new()
+var _new_ConvertCoord := preload("res://library/ConvertCoord.gd").new()
+var _new_GroupName := preload("res://library/GroupName.gd").new()
+
+var _pc: Sprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_node("../InitWorld").sprite_created.connect(_on_InitWorld_sprite_created)
+	set_process_unhandled_input(false)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_InitWorld_sprite_created(sprite: Sprite2D):
+	if sprite.is_in_group(_new_GroupName.PC):
+		_pc = sprite
+		set_process_unhandled_input(true)
+
+func _unhandled_input(event: InputEvent) -> void:
+	print("pc: {0}".format([_pc]))
+	var source: Array = _new_ConvertCoord.vector_to_array(_pc.position)
+	var x: int = source[0]
+	var y: int = source[1]
+
+	if event.is_action_pressed(_new_InputName.MOVE_LEFT):
+		x -= 1
+	elif event.is_action_pressed(_new_InputName.MOVE_RIGHT):
+		x += 1
+	elif event.is_action_pressed(_new_InputName.MOVE_UP):
+		y -= 1
+	elif event.is_action_pressed(_new_InputName.MOVE_DOWN):
+		y += 1
+
+
+	_pc.position = _new_ConvertCoord.index_to_vector(x, y)
